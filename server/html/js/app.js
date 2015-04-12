7ea2864a356945f11f1f2a9e2a65d842f9cdae94
Joshua Tucker <j.allentucker@gmail.com>
2015-04-12 01:19:57 -0700

    figured out how to use lodash in app.js

--- server/html/js/app.js
+++ server/html/js/app.js
@@ -1,3 +1,6 @@
+// var fs = require('browserify-fs');
+var _ = require('lodash');
+
 App = Ember.Application.create();
 
 App.Router.map(function() {
@@ -6,6 +9,34 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
+    // fs.readFile('adp.txt', { encoding: 'utf8' }, function(err, adpData) {
+    //   var adpPlayerStrings = adpData.split('\r\n');
+    //   // var players = adpPlayerStrings.map(function(adpPlayerString) {
+    //   //   var adpPlayerArray = adpPlayerString.split('\t');
+    //   //   var adpPlayerName = adpPlayerArray[1].split(',')[0].trim();
+    //   //   var adpPlayerObject = {
+    //   //     name: adpPlayerName,
+    //   //     adp: parseFloat(adpPlayerArray[3])
+    //   //   };
+    //   //   return adpPlayerObject;
+    //   // });
+    //   var players = [
+    //     {
+    //       name: "Mike Trout",
+    //       adp: 1.2
+    //     }, {
+    //       name: "Clayton Kershaw",
+    //       adp: 3.4
+    //     }, {
+    //       name: "Paul Goldschmidt",
+    //       adp: 5.6
+    //     }, {
+    //       name: "Miguel Cabrera",
+    //       adp: 8
+    //     }
+    //   ];
+    //   return players;
+    // });
     var players = [
       {
         name: "Mike Trout",
@@ -21,6 +52,6 @@ App.IndexRoute = Ember.Route.extend({
         adp: 8
       }
     ];
-    return players;
+    return _.rest(players);
   }
 });
